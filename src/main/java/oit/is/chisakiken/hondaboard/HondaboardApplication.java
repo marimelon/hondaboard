3f8e27d1c887a7b7d72a4b97f5cef5f3394e476b
marimelon <29140293+marimelon@users.noreply.github.com>
2020-11-10 14:55:55 +0900

    init

---
+++ src/main/java/oit/is/chisakiken/hondaboard/HondaboardApplication.java
@@ -0,0 +1,13 @@
+package oit.is.chisakiken.hondaboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HondaboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HondaboardApplication.class, args);
+	}
+
+}
