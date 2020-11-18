228352e5175c5f491f06f7f043ac7730fe10a518
marimelon <29140293+marimelon@users.noreply.github.com>
2020-11-18 16:25:10 +0900

    er図に対応するschema.sqlを作成

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,3 +3,24 @@ CREATE TABLE user (
     name CHAR NOT NULL,
     password CHAR
 );
+
+CREATE TABLE room(
+    id IDENTITY,
+    name CHAR NOT NULL
+);
+
+CREATE TABLE roomuser(
+    room_id INT NOT NULL,
+    user_id INT NOT NULL,
+    role CHAR NOT NULL,
+    PRIMARY KEY(room_id, user_id)
+);
+
+Create Table comment(
+    id IDENTITY,
+    room_id INT NOT NULL,
+    user_id INT NOT NULL,
+    content CHAR NOT NULL,
+    date TIMESTAMP NOT NULL
+);
+
