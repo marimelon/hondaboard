82a8727c0d6f4a6dfd0297b30ad3af43397f73e6
marimelon <29140293+marimelon@users.noreply.github.com>
2020-12-17 17:19:13 +0900

    Last-Event-IDを利用した未送信メッセージの送信

--- src/main/java/oit/is/chisakiken/hondaboard/dto/Message.java
+++ src/main/java/oit/is/chisakiken/hondaboard/dto/Message.java
@@ -2,10 +2,19 @@ package oit.is.chisakiken.hondaboard.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import oit.is.chisakiken.hondaboard.model.Comment;
+import oit.is.chisakiken.hondaboard.model.LoginUser;
 
 @Data
 @AllArgsConstructor
 public class Message {
+    int id;
     String username;
     String content;
+
+    public Message(LoginUser user,Comment comment){
+        this.id = comment.getId();
+        this.username = user.getName();
+        this.content = comment.getContent();
+    }
 }
